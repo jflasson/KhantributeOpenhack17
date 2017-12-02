4ebaadf7252a639e71cf4acd2c4d3ee0b3fe6089
johan.forsslasson <johan.forsslasson@sigmaconnectivity.se>
2017-12-02 23:06:05 +0100

    added new progressbar etc

--- android/KhanProof/app/src/main/java/com/example/johan/khanproof/RatingActivity.java
+++ android/KhanProof/app/src/main/java/com/example/johan/khanproof/RatingActivity.java
@@ -18,38 +18,30 @@ public class RatingActivity extends AppCompatActivity {
     Button maybeButton;
     Button badButton;
     LinearLayout levelprogressbar;
-    static TextView securePointsTv;
-    static TextView potentialPointsTv;
-    static TextView pointsleftTv;
     int securePoints;
     int potentialPoints;
     int leftPoints;
+    static int minprogress;
+    static int maxprogress;
+    static ProgressBar levelProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_rating);
-        originalText = (TextView) findViewById(R.id.originaltext);
-        goodButton = (Button) findViewById(R.id.btnGood);
-        maybeButton = (Button) findViewById(R.id.btnMaybe);
-        badButton = (Button) findViewById(R.id.btnBad);
-        securePointsTv = (TextView) findViewById(R.id.securepoints);
-        potentialPointsTv = (TextView) findViewById(R.id.potentialpoints);
-        pointsleftTv = (TextView) findViewById(R.id.pointstoleftlevel);
-        ProgressBar levelProgressBar = (ProgressBar) findViewById(R.id.levelProgressBar);
+        originalText = findViewById(R.id.originaltext);
+        levelProgressBar = findViewById(R.id.levelProgressBar);
         levelProgressBar.setProgress(25);
         levelProgressBar.setSecondaryProgress(55);
     }
 
     public void onClickGood(View view){
         toast("Good pressed");
-        setLevelBar(90, 5, 5);
     }
 
     public void onClickMaybe(View view){
         toast("Maybe pressed");
-        setLevelBar(25, 55, 30);
     }
 
     public void onClickBad(View view){
@@ -60,9 +52,16 @@ public class RatingActivity extends AppCompatActivity {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 
-    static public void setLevelBar(int secure, int potential, int left) {
-        securePointsTv.setLayoutParams(new TableLayout.LayoutParams(securePointsTv.getHeight(), securePointsTv.getWidth(), secure));
-        potentialPointsTv.setLayoutParams(new TableLayout.LayoutParams(potentialPointsTv.getHeight(), potentialPointsTv.getWidth(), potential));
-        pointsleftTv.setLayoutParams(new TableLayout.LayoutParams(pointsleftTv.getHeight(), pointsleftTv.getWidth(), left));
+    static public void setLevelBarMaxAndMin(int min, int max) {
+        minprogress = min;
+        maxprogress = max;
+        levelProgressBar.setMax(max - min);
+    }
+
+    static public void setLevelBarPrimaryProgress(int progress) {
+        levelProgressBar.setProgress(progress - minprogress);
+    }
+    static public void setLevelBarSecondaryProgress(int progress) {
+        levelProgressBar.setSecondaryProgress(progress - minprogress);
     }
 }
