9f32542565e3e500a7f9a467707441c05ca5c889
johan.forsslasson <johan.forsslasson@sigmaconnectivity.se>
2017-12-03 11:53:36 +0100

    final submission commit

---
+++ android/KhanProof/app/src/main/java/com/example/johan/khanproof/StringPair.java
@@ -0,0 +1,15 @@
+package com.example.johan.khanproof;
+
+/**
+ * Created by johan on 2017-12-03.
+ */
+
+public class StringPair {
+    String original;
+    String translation;
+
+    public  StringPair(String original, String translation){
+        this.original = original;
+        this.translation = translation;
+    }
+}
