ba6e7b11f65aa3a611a12ff529f89d7a8145b220
Tomas Sjöberg <tomas.sjoberg@verisure.com>
2017-12-03 11:10:08 +0100

    Second (dummy) version of backend

--- backend/routes/texts.js
+++ backend/routes/texts.js
@@ -7,12 +7,79 @@ crowdin = new crowdin({
 	endpointUrl: 'https://api.crowdin.net/api/project/tomastest'
 });
 
-router.get('/getString', function(req, res, next) {
+router.get('/string', function(req, res, next) {
 	const dummyData = {
+		fileName: 'other.po',
 		original: 'This is an example sentence.',
 		sv_SE: 'Det här är en exempel mening.'
 	};
-  res.send(dummyData);
+	res.setHeader("Content-Type",'application/json');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.send(dummyData);
+});
+
+router.post('/string', function(req, res, next) {
+	//TODO: send all + score (-1,0,1)
+	req.acceptsEncodings('application/json');
+});
+
+router.get('/score', function(req, res, next) {
+	const dummyData = {
+		score: 200,
+		pending: 5,
+		place: 3
+	};
+	res.setHeader("Content-Type",'application/json');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.send(dummyData);
+});
+
+router.get('/topList', function(req, res, next) {
+	const dummyData = [
+		{
+			name: 'First person',
+			score: 300
+		},
+		{
+			name: 'Second person',
+			score: 250
+		},
+		{
+			name: 'Another person',
+			score: 200
+		},
+		{
+			name: 'Another person',
+			score: 150
+		},
+		{
+			name: 'Another person',
+			score: 100
+		},
+		{
+			name: 'Another person',
+			score: 90
+		},
+		{
+			name: 'Another person',
+			score: 80
+		},
+		{
+			name: 'Another person',
+			score: 70
+		},
+		{
+			name: 'Another person',
+			score: 60
+		},
+		{
+			name: 'Another person',
+			score: 50
+		}
+	];
+	res.setHeader("Content-Type",'application/json');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.send(dummyData);
 });
 
 function getRandomString() {
